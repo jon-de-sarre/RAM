994dfbbfd5404aed58357881ee7290cc158978ab
Dave Cheong <dave@mobiusly.com>
2016-07-22 11:50:21 +1000

    Moved app.component out

---
+++ frontend/typescript/app/app.component.ts
@@ -0,0 +1,36 @@
+import 'ng2-bootstrap';
+import {Component} from '@angular/core';
+import {HTTP_PROVIDERS} from '@angular/http';
+import {ROUTER_DIRECTIVES} from '@angular/router';
+
+import {RAMServices} from '../commons/ram-services';
+import {RAMRestService} from '../services/ram-rest.service';
+import {RAMModelHelper} from '../commons/ram-model-helper';
+import {RAMRouteHelper} from '../commons/ram-route-helper';
+import {RAMNavService} from '../services/ram-nav.service';
+import {RAMConstantsService} from '../services/ram-constants.service';
+
+import {BannerComponent} from '../components/commons/banner/banner.component';
+import {BannerService} from '../components/commons/banner/banner.service';
+import {ErrorComponent} from '../components/commons/error/error.component';
+import {ErrorService} from '../components/commons/error/error.service';
+
+@Component({
+    selector: 'ram-app',
+    templateUrl: 'app.component.html',
+    directives: [ROUTER_DIRECTIVES, ErrorComponent, BannerComponent],
+    providers: [
+        HTTP_PROVIDERS,
+        RAMServices,
+        RAMRestService,
+        RAMModelHelper,
+        RAMRouteHelper,
+        RAMNavService,
+        RAMConstantsService,
+        BannerService,
+        ErrorService
+    ]
+})
+
+export class AppComponent {
+}
