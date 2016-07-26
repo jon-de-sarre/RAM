ff2215a76adc868c7172a21e135a203b65fd7977
Dave Cheong <dave@mobiusly.com>
2016-07-26 18:21:13 +1000

    RAM-438 Accommodate principal, work in progress

--- backend/typescript/controllers/forgeRock.simulator.middleware.ts
+++ backend/typescript/controllers/forgeRock.simulator.middleware.ts
@@ -3,32 +3,36 @@ import * as colors from 'colors';
 import {Request, Response} from 'express';
 import {Headers} from './headers';
 import {SecurityHelper} from './security.middleware';
+import {AgencyUsersSeeder} from '../seeding/seed-agency-users';
 import {IIdentity, IdentityModel} from '../models/identity.model';
+import {IAgencyUser} from '../../../commons/RamAPI2';
 
 class ForgeRockSimulator {
 
     public prepareRequest(): (req: Request, res: Response, next: () => void) => void {
         const self = this;
         return (req: Request, res: Response, next: () => void) => {
-            const credentialsFromAuthenticationSimulator = req.body.credentials;
-            const idValueFromCookie = SecurityHelper.getValueFromCookies(req, Headers.AuthToken);
-            if (credentialsFromAuthenticationSimulator) {
-                // log in from development only login form
-                IdentityModel.findByIdValue(credentialsFromAuthenticationSimulator)
-                    .then(self.resolve(req, res, next))
-                    .catch(self.reject(res, next));
-            } else if (idValueFromCookie) {
-                // log in from cookie
-                IdentityModel.findByIdValue(idValueFromCookie)
-                    .then(self.resolve(req, res, next))
-                    .catch(self.reject(res, next));
+            const idFromAuthenticationSimulator = req.body.credentials;
+            const idFromCookie = SecurityHelper.getValueFromCookies(req, Headers.AuthToken);
+            const id = idFromAuthenticationSimulator ? idFromAuthenticationSimulator : idFromCookie;
+            if (id) {
+                const agencyUser = AgencyUsersSeeder.findById(id);
+                if (!agencyUser) {
+                    IdentityModel.findByIdValue(id)
+                        .then(self.resolveForIdentity(req, res, next))
+                        .catch(self.reject(res, next));
+                } else {
+                    Promise.resolve(agencyUser)
+                        .then(self.resolveForAgencyUser(req, res, next))
+                        .catch(self.reject(res, next));
+                }
             } else {
                 next();
             }
         };
     }
 
-    private resolve(req: Request, res: Response, next: () => void) {
+    private resolveForIdentity(req: Request, res: Response, next: () => void) {
         return (identity?: IIdentity) => {
             if (identity) {
                 logger.info(colors.red(`Setting ${Headers.IdentityIdValue}: ${identity.idValue}`));
@@ -51,9 +55,26 @@ class ForgeRockSimulator {
         };
     }
 
+    private resolveForAgencyUser(req: Request, res: Response, next: () => void) {
+        return (agencyUser?: IAgencyUser) => {
+            if (agencyUser) {
+                logger.info(colors.red(`Setting ${Headers.AgencyUserLoginId}: ${agencyUser.id}`));
+                let programRolesString = '';
+                for (let i = 0; i < agencyUser.programRoles; i = i + 1) {
+                    let programRole = agencyUser.programRoles[i];
+                    programRolesString += (i > 0 ? ',' : '') + programRole.program + ':' + programRole.role;
+                }
+                res.locals[Headers.GivenName] = agencyUser.givenName;
+                res.locals[Headers.FamilyName] = agencyUser.familyName;
+                res.locals[Headers.AgencyUserProgramRoles] = programRolesString;
+            }
+            next();
+        };
+    }
+
     private reject(res: Response, next: () => void) {
         return (): void => {
-            logger.info('Unable to look up identity!');
+            logger.info('Unable to look up identity or agency user!');
             res.status(401);
             res.send({});
         };
