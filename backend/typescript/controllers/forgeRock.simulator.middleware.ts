4509a85252a2c64822aac630e4f2b0d30815a48c
Dave Cheong <dave@mobiusly.com>
2016-07-26 17:12:33 +1000

    RAM-438 Accommodate principal, work in progress

--- backend/typescript/controllers/forgeRock.simulator.middleware.ts
+++ backend/typescript/controllers/forgeRock.simulator.middleware.ts
@@ -7,9 +7,9 @@ import {IIdentity, IdentityModel} from '../models/identity.model';
 
 class ForgeRockSimulator {
 
-    public prepareRequest():(req:Request, res:Response, next:() => void) => void {
+    public prepareRequest(): (req: Request, res: Response, next: () => void) => void {
         const self = this;
-        return (req:Request, res:Response, next:() => void) => {
+        return (req: Request, res: Response, next: () => void) => {
             const credentialsFromAuthenticationSimulator = req.body.credentials;
             const idValueFromCookie = SecurityHelper.getValueFromCookies(req, Headers.AuthToken);
             if (credentialsFromAuthenticationSimulator) {
@@ -28,8 +28,8 @@ class ForgeRockSimulator {
         };
     }
 
-    private resolve(req:Request, res:Response, next:() => void) {
-        return (identity?:IIdentity) => {
+    private resolve(req: Request, res: Response, next: () => void) {
+        return (identity?: IIdentity) => {
             if (identity) {
                 logger.info(colors.red(`Setting ${Headers.IdentityIdValue}: ${identity.idValue}`));
                 res.locals[Headers.IdentityIdValue] = identity.idValue;
@@ -51,8 +51,8 @@ class ForgeRockSimulator {
         };
     }
 
-    private reject(res:Response, next:() => void) {
-        return ():void => {
+    private reject(res: Response, next: () => void) {
+        return (): void => {
             logger.info('Unable to look up identity!');
             res.status(401);
             res.send({});
