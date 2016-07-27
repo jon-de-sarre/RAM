9eb0fb9bc474c1d3aeebf1456bb6e38dad813c30
Dave Cheong <dave@mobiusly.com>
2016-07-27 10:59:06 +1000

    RAM-440 Updated postman

--- backend/typescript/controllers/agencyUser.controller.ts
+++ backend/typescript/controllers/agencyUser.controller.ts
@@ -1,15 +1,11 @@
 import {Router, Request, Response} from 'express';
-import {security} from './security.middleware';
 import {sendList, sendError, sendNotFoundError, validateReqSchema} from './helpers';
-import {Headers} from './headers';
 import {conf} from '../bootstrap';
-import {IAgencyUser} from '../models/agencyUser.model';
 import {AgencyUsersSeeder} from '../seeding/seed-agency-users';
 
 export class AgencyUserController {
 
     private search = async(req: Request, res: Response) => {
-        const principal = res.locals[Headers.Principal];
         const schema = {};
         validateReqSchema(req, schema)
             .then((req:Request) => AgencyUsersSeeder.all())
