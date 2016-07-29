f8e594d6be20b3f47838cc79b6eb29bc03bae74e
Dave Cheong <dave@mobiusly.com>
2016-07-29 12:17:12 +1000

    RAM-251 Added /v1/agencyUser/me

--- backend/typescript/controllers/agencyUser.controller.ts
+++ backend/typescript/controllers/agencyUser.controller.ts
@@ -1,9 +1,23 @@
 import {Router, Request, Response} from 'express';
-import {sendList, sendError, sendNotFoundError, validateReqSchema} from './helpers';
+import {security} from './security.middleware';
+import {sendResource, sendList, sendError, sendNotFoundError, validateReqSchema} from './helpers';
 import {conf} from '../bootstrap';
+import {Headers} from './headers';
 import {AgencyUsersSeeder} from '../seeding/seed-agency-users';
+import {IAgencyUser} from '../models/agencyUser.model';
 
 export class AgencyUserController {
+
+    private findMe = async(req: Request, res: Response) => {
+        const agencyUser = res.locals[Headers.AgencyUser];
+        const schema = {};
+        validateReqSchema(req, schema)
+            .then((req: Request) => agencyUser ? agencyUser : null)
+            .then((model: IAgencyUser) => model ? model.toDTO() : null)
+            .then(sendResource(res))
+            .then(sendNotFoundError(res))
+            .catch(sendError(res));
+    };
 
     private search = async(req: Request, res: Response) => {
         const schema = {};
@@ -16,6 +30,10 @@ export class AgencyUserController {
     };
 
     public assignRoutes = (router: Router) => {
+
+        router.get('/v1/agencyUser/me',
+            security.isAuthenticatedAsAgencyUser,
+            this.findMe);
 
         if (conf.devMode) {
             router.get('/v1/agencyUsers', this.search);
