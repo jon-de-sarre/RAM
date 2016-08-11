bec4d803051e878f645bab78d7a7d721ea1968ca
Dave Cheong <dave@mobiusly.com>
2016-08-11 14:27:35 +1000

    RAM-308 Restructuring constants

--- frontend/typescript/services/ram-constants.service.ts
+++ frontend/typescript/services/ram-constants.service.ts
@@ -3,10 +3,6 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class RAMConstantsService {
 
-    public PageSizeOptions = [5, 10, 25, 100];
-    public DefaultPageSize = 5;
-    public PartyId = '5719bc5d65cae16c197e1ecd';
-
     public AUSkey = {
         DEVICE_TYPE: 'DEVICE'
     };
