4075456f860eda675fcb63f5ebc6a69012da170b
Dave Cheong <dave@mobiusly.com>
2016-08-12 10:08:38 +1000

    RAM-4 Principal with references to agency user and identity

--- backend/typescript/controllers/security.middleware.ts
+++ backend/typescript/controllers/security.middleware.ts
@@ -115,9 +115,7 @@ class Security {
                         ));
                     }
                 }
-                res.locals[Headers.Principal] = new Principal(idValue, displayName, true);
-                res.locals[Headers.PrincipalIdValue] = idValue;
-                res.locals[Headers.AgencyUser] = new AgencyUser(
+                const agencyUser = new AgencyUser(
                     idValue,
                     givenName,
                     familyName,
@@ -125,6 +123,9 @@ class Security {
                     this.getValueFromHeaderLocalsOrCookie(req, res, Headers.AgencyUserAgency),
                     programRoles
                 );
+                res.locals[Headers.Principal] = new Principal(idValue, displayName, true, agencyUser, null);
+                res.locals[Headers.PrincipalIdValue] = idValue;
+                res.locals[Headers.AgencyUser] = agencyUser;
             }
         };
     }
@@ -133,7 +134,7 @@ class Security {
         return (identity?: IIdentity) => {
             logger.info('Identity context:', (identity ? colors.magenta(identity.idValue) : colors.red('[not found]')));
             if (identity) {
-                res.locals[Headers.Principal] = new Principal(identity.idValue, identity.profile.name._displayName, false);
+                res.locals[Headers.Principal] = new Principal(identity.idValue, identity.profile.name._displayName, false, null, identity);
                 res.locals[Headers.PrincipalIdValue] = identity.idValue;
                 res.locals[Headers.Identity] = identity;
                 res.locals[Headers.IdentityIdValue] = identity.idValue;
--- backend/typescript/models/principal.model.ts
+++ backend/typescript/models/principal.model.ts
@@ -1,26 +1,32 @@
-import {
-    Principal as PrincipalDTO
-} from '../../../commons/RamAPI';
+import {IAgencyUser} from './agencyUser.model';
+import {IIdentity} from './identity.model';
+import {Principal as PrincipalDTO} from '../../../commons/RamAPI';
 
 export interface IPrincipal {
     id: string;
     displayName: string;
     agencyUserInd: boolean;
+    agencyUser?: IAgencyUser;
+    identity?: IIdentity;
     toDTO(): Promise<PrincipalDTO>;
 }
 
 export class Principal implements IPrincipal {
 
-    constructor(public id:string,
-                public displayName:string,
-                public agencyUserInd:boolean) {
+    constructor(public id: string,
+                public displayName: string,
+                public agencyUserInd: boolean,
+                public agencyUser?: IAgencyUser,
+                public identity?: IIdentity) {
     }
 
-    public toDTO(): Promise<PrincipalDTO> {
+    public async toDTO(): Promise<PrincipalDTO> {
         return Promise.resolve(new PrincipalDTO(
             this.id,
             this.displayName,
-            this.agencyUserInd
+            this.agencyUserInd,
+            this.agencyUser ? await this.agencyUser.toDTO() : null,
+            this.identity ? await this.identity.toDTO() : null
         ));
     }
 
