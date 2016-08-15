2b17385f4f358f0c70c26d144dd825b3d66d9188
Dave Cheong <dave@mobiusly.com>
2016-08-15 12:28:16 +1000

    RAM-4 Added context provider

---
+++ backend/typescript/providers/context.provider.ts
@@ -0,0 +1,113 @@
+import {logger} from '../logger';
+import {Request, Response, NextFunction} from 'express';
+import {Headers} from '../controllers/headers';
+import {Namespace} from 'continuation-local-storage';
+import {IIdentity, ErrorResponse, IAgencyUser, IPrincipal} from '../../../commons/RamAPI';
+
+import cls = require('continuation-local-storage');
+import clsDomainsPromise = require('cls-domains-promise');
+
+const DEFAULT_NAMESPACE_KEY = 'default';
+
+class Context {
+
+    public static init(): Namespace {
+        let namespace = cls.createNamespace(DEFAULT_NAMESPACE_KEY);
+        clsDomainsPromise(namespace, Promise.prototype);
+        return namespace;
+    };
+
+    public static begin(req: Request, res: Response, next: NextFunction) {
+        let namespace = Context.getNamespace();
+        namespace.run(() => {
+            Context.setup(req, res, next);
+        });
+    };
+
+    public static setup(req: Request, res: Response, next: () => void) {
+        console.log('Setting up context ...');
+        console.log('Me orig:', res.locals[Headers.IdentityIdValue]);
+        for (let key of Object.keys(res.locals)) {
+            // keys should be lowercase, but let's make sure
+            const keyLower = key.toLowerCase();
+            // if it's an application key, copy it to the context
+            if (keyLower.startsWith(Headers.Prefix)) {
+                Context.set(keyLower, res.locals[key]);
+            }
+        }
+        next();
+    }
+
+    public static get(key: string) {
+        return Context.getNamespace().get(key);
+    }
+
+    public static set(key: string, value: any) {
+        return Context.getNamespace().set(key, value);
+    }
+
+    public static getAuthenticatedIdentityIdValue(): string {
+        return Context.get(Headers.IdentityIdValue);
+    }
+
+    public static getAuthenticatedIdentity(): IIdentity {
+        return Context.get(Headers.Identity);
+    }
+
+    public static getAuthenticatedAgencyUserLoginId(res: Response): string {
+        return Context.get(Headers.AgencyUserLoginId);
+    }
+
+    public static getAuthenticatedAgencyUser(res: Response): IAgencyUser {
+        return Context.get(Headers.AgencyUser);
+    }
+
+    public static getAuthenticatedPrincipalIdValue(res: Response): string {
+        return Context.get(Headers.PrincipalIdValue);
+    }
+
+    public static getAuthenticatedPrincipal(res: Response): IPrincipal {
+        return Context.get(Headers.Principal);
+    }
+
+    public static isAuthenticated(req: Request, res: Response, next: () => void) {
+            const id = res.locals[Headers.PrincipalIdValue];
+            if (id) {
+                next();
+            } else {
+                logger.error('Unable to invoke route requiring authentication'.red);
+                res.status(401);
+                res.send(new ErrorResponse('Not authenticated.'));
+            }
+        }
+
+    public static isAuthenticatedAsAgencyUser(req: Request, res: Response, next: () => void) {
+        const principal = res.locals[Headers.Principal];
+        if (principal && principal.agencyUserInd) {
+            next();
+        } else {
+            logger.error('Unable to invoke route requiring agency user'.red);
+            res.status(401);
+            res.send(new ErrorResponse('Not authenticated as agency user.'));
+        }
+    }
+
+    public static getAuthenticatedABN(res: Response): string {
+        return res.locals[Headers.ABN];
+    }
+
+    public static getAuthenticatedAUSkey(res: Response): string {
+        return res.locals[Headers.AUSkey];
+    }
+
+    public static getAuthenticatedClientAuth(res: Response): string {
+        return res.locals[Headers.ClientAuth];
+    }
+
+    public static getNamespace() {
+        return cls.getNamespace(DEFAULT_NAMESPACE_KEY);
+    };
+
+}
+
+export const context = Context;
