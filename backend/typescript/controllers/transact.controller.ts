8cd18b195000592b9bbd02fdb7b811ed13a2f0c3
Dave Cheong <dave@mobiusly.com>
2016-08-16 14:33:10 +1000

    RAM-6 Postman, RamAPI DTOs

--- backend/typescript/controllers/transact.controller.ts
+++ backend/typescript/controllers/transact.controller.ts
@@ -1,23 +1,49 @@
 import {Router, Request, Response} from 'express';
 import {context} from '../providers/context.provider';
 import {sendResource, sendError, sendNotFoundError, validateReqSchema} from './helpers';
+import {ITransactRequest, TransactResponse} from '../../../commons/RamAPI';
 //import {Headers} from './headers';
 
 export class TransactController {
 
     private allowed = async(req: Request, res: Response) => {
-        const schema = {};
+
+        const schema = {
+            'clientABN': {
+                in: 'body',
+                notEmpty: true,
+                errorMessage: 'Client ABN is not valid'
+            },
+            'ssid': {
+                in: 'body',
+                notEmpty: true,
+                errorMessage: 'SSID is not valid'
+            },
+            'agencyService': {
+                in: 'body',
+                notEmpty: true,
+                errorMessage: 'Agency Service is not valid'
+            }
+        };
+
         const auskey = context.getAuthenticatedAUSkey();
         const abn = context.getAuthenticatedABN();
+        const request = req.body as ITransactRequest;
+
+        console.log('request body =', request);
+
         console.log('auskey =', auskey, ', abn =', abn);
+
         validateReqSchema(req, schema)
             .then((req: Request) => {
-                return {allowed: true};
+                const allowed = true; // todo compute allowed flag
+                return new TransactResponse(request, allowed);
             })
             //.then((model) => model ? model.toDTO() : null)
             .then(sendResource(res))
             .then(sendNotFoundError(res))
             .catch(sendError(res));
+
     };
 
     public assignRoutes = (router: Router) => {
