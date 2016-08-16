65e7090c70938c23c2adcb576b81a9b297d58350
Dave Cheong <dave@mobiusly.com>
2016-08-16 12:18:46 +1000

    RAM-344 Added todo comment

--- frontend/typescript/pages/businesses/businesses.component.ts
+++ frontend/typescript/pages/businesses/businesses.component.ts
@@ -32,6 +32,7 @@ import {
 
 // todo this component needs to be rewritten to be compliant with project standards
 // todo this component doesn't seem to render with search text via a browser reload
+// todo this component shouldn't have parties$ (template shouldn't use |async)
 export class BusinessesComponent extends AbstractPageComponent {
 
     public filter: FilterParams;
